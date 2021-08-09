a19477562028621e4f17be89fa450202f9e9604a
David Lounsbrough <david.lounsbrough@gmail.com>
2021-08-09 17:15:50 -0500

    Updating PM2 config

--- pm2.config.js
+++ pm2.config.js
@@ -4,6 +4,7 @@ module.exports = {
     cmd: 'main.py',
     args: '--lot=2000 TSLA FB AAPL',
     autorestart: false,
+    cron_restart: '0 3 * * *',
     watch: false,
     interpreter: 'python3'
   }]
