051c47279d2e980a9323f954f1a6a415c6298668
David Lounsbrough <david.lounsbrough@gmail.com>
2021-08-10 22:24:45 -0500

    Allow PM2 to restart

--- pm2.config.js
+++ pm2.config.js
@@ -2,7 +2,6 @@ module.exports = {
   apps : [{
     name: 'alpaca-trading-bot',
     cmd: 'main.py',
-    autorestart: false,
     cron_restart: '0 3 * * *',
     watch: false,
     interpreter: 'python3'
