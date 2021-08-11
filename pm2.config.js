505b5fb486dc38654f420135483cd3ae35436bfe
David Lounsbrough <david.lounsbrough@gmail.com>
2021-08-10 23:10:57 -0500

    Wait 10 seconds between restarts

--- pm2.config.js
+++ pm2.config.js
@@ -2,6 +2,7 @@ module.exports = {
   apps : [{
     name: 'alpaca-trading-bot',
     cmd: 'main.py',
+    restart_delay: 10000,
     cron_restart: '0 3 * * *',
     watch: false,
     interpreter: 'python3'
